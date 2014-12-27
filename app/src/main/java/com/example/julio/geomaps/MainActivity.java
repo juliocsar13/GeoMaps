e85122eda1e058251bc28696c51a880a115b216e
juliocsar13 <jc_redcsar@hotmail.com>
2014-12-27 16:38:59 -0500

    Initial commit

---
+++ app/src/main/java/com/example/julio/geomaps/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.julio.geomaps;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+public class MainActivity extends ActionBarActivity {
+
+
+    private Button btnActualizar;
+    private Button btnDesactivar;
+    private TextView lblLatitud;
+    private TextView lblLongitud;
+    private TextView lblPrecision;
+    private TextView lblEstado;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnActualizar = (Button) findViewById(R.id.BtnActualizar);
+        btnDesactivar = (Button) findViewById(R.id.BtnDesactivar);
+        lblLatitud = (TextView) findViewById(R.id.LblPosLatitud);
+        lblLongitud = (TextView) findViewById(R.id.LblPosLongitud);
+        lblPrecision = (TextView) findViewById(R.id.LblPosPrecision);
+        lblEstado = (TextView) findViewById(R.id.LblEstado);
+
+        btnActualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                actualizarPosicion();
+            }
+        });
+
+        btnDesactivar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                locationManager.removeUpdates(locationListener);
+            }
+        });
+    }
+        private void actualizarPosicion() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        muestraPosicion(location);
+        locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                muestraPosicion(location);
+            }
+            public void onProviderDisabled(String provider) {
+                lblEstado.setText("Provider OFF");
+            }
+            public void onProviderEnabled(String provider) {
+                lblEstado.setText("Provider ON");
+            }
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+                Log.i("LocAndroid", "Provider Status: " + status);
+                lblEstado.setText("Provider Status: " + status);
+            }
+        };
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 0, locationListener);
+        }
+        private void muestraPosicion(Location loc) {
+            if (loc != null) {
+                lblLatitud.setText("Latitud: " + String.valueOf(loc.getLatitude()));
+                lblLongitud.setText("Longitud: " + String.valueOf(loc.getLongitude()));
+                lblPrecision.setText("Precision: " + String.valueOf(loc.getAccuracy()));
+                Log.i("LocAndroid", String.valueOf(loc.getLatitude() + " - " + String.valueOf(loc.getLongitude())));
+            } else {
+                lblLatitud.setText("Latitud: (sin_datos)");
+                lblLongitud.setText("Longitud: (sin_datos)");
+                lblPrecision.setText("Precision: (sin_datos)");
+            }
+        }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
